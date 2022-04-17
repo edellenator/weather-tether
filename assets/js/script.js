a7a5790ca7ea2ab5240e1695e495dfc4b5938c26
Erik Dell <edellenator@gmail.com>
2022-04-17 14:15:18 -0700

    Jumbotron displays City Search Query, Date, weather icon, and all weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,25 @@ btnEl.addEventListener('click', function(event){
             console.log('city name is invalid')
         }
         });
-
-    // 
-    // console.log(weatherApiUrl);
 })
 
 var getWeatherData = function(lat, lon) {
-    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + weatherApiKey;
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + weatherApiKey;
     fetch(weatherApiUrl).then(function(response){
         if (response.ok) {
             response.json().then(function(weather){
-                console.log(weather.daily)
+                console.log(weather)
+                var unixTime = weather.current.dt;
+                var fullDate = new Date (unixTime*1000);
+                var date = fullDate.toLocaleDateString('en-US')
+                var temp = weather.current.temp;
+                var wind = weather.current.wind_speed;
+                var uvi = weather.current.uvi;
+                var hum = weather.current.humidity
+                var icon = weather.current.weather[0].icon
+
+                console.log(date);
+                displayJumbotron(temp, wind, hum, uvi, date, icon)
             })
         }
         else {
@@ -39,4 +47,26 @@ var getWeatherData = function(lat, lon) {
         });
 }
 
-// var displayJumbotron = function()
+var displayJumbotron = function(temp, wind, hum, uv, date, icon) {
+    const tempEl = document.getElementById('temp');
+    const windEl = document.getElementById('wind');
+    const humEl = document.getElementById('humidity');
+    const uvEl = document.getElementById('uv');
+    const dateEl = document.getElementById('today');
+    const iconEl = document.getElementById('weather-icon');
+    iconEl.innerHTML = '<img src="http://openweathermap.org/img/wn/' + icon + '@2x.png">'
+    dateEl.textContent = date;
+    tempEl.textContent = temp;
+    windEl.textContent = wind;
+    humEl.textContent = hum;
+    uvEl.textContent = uv;
+}
+
+// var displayForecast = function (date, temp, wind, hum) {
+//     const foreTemp = document.querySelectorAll('.fore-temp');
+//     const foreWind = document.querySelectorAll('.fore-wind')
+//     const foreDate = document.querySelectorAll('.fore-date')
+//     const foreHum = document.querySelectorAll('.fore-humidity')
+//     for (var i = 0; i < )
+
+// }
