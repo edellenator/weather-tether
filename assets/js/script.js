b26d421270d1efaf90ccd82ea0b6cabea32cfed3
Erik Dell <edellenator@gmail.com>
2022-04-17 15:58:26 -0700

    5 day forecast displayed for selected city

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,19 @@ btnEl.addEventListener('click', function(event){
     var geoApiUrl = "https://nominatim.openstreetmap.org/search.php?=&city=" + cityString + "&format=jsonv2&limit=1"
     fetch(geoApiUrl).then(function(response){
         if (response.ok) {
-            response.json().then(function(city){
-            getWeatherData(city[0].lat, city[0].lon)
+            response.json().then(function(city) {
+                getWeatherData(city[0].lat, city[0].lon);
             })
         }
         else {
-            console.log('city name is invalid')
+            alert('city name is invalid')
         }
-        });
-})
+    })
+    .catch(function(error){
+        alert('It appears that all of the satellites have crashed from the sky and we can no longer retrieve any weather data')
+    });
+        
+});
 
 var getWeatherData = function(lat, lon) {
     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + weatherApiKey;
@@ -28,6 +32,7 @@ var getWeatherData = function(lat, lon) {
         if (response.ok) {
             response.json().then(function(weather){
                 console.log(weather)
+                // store all data from weather api as variables
                 var unixTime = weather.current.dt;
                 var fullDate = new Date (unixTime*1000);
                 var date = fullDate.toLocaleDateString('en-US')
@@ -36,9 +41,36 @@ var getWeatherData = function(lat, lon) {
                 var uvi = weather.current.uvi;
                 var hum = weather.current.humidity
                 var icon = weather.current.weather[0].icon
-
-                console.log(date);
+                // apply weather data to jumbotron
                 displayJumbotron(temp, wind, hum, uvi, date, icon)
+                var foreTempArr = []
+                for (var i = 0; i < weather.daily.length; i++){
+                    foreTempArr.push(weather.daily[i].temp.max)
+                }
+
+                var foreDateArr = [];
+                for (var i = 0; i < weather.daily.length; i++){
+                    var unixForeTime = weather.daily[i].dt;
+                    var fullForeDate = new Date (unixForeTime*1000);
+                    var hrDate = fullForeDate.toLocaleDateString('en-US'); 
+                    foreDateArr.push(hrDate)
+                }
+
+                var foreWindArr = [];
+                for (var i = 0; i < weather.daily.length; i++){
+                    foreWindArr.push(weather.daily[i].wind_speed)
+                }
+               
+                var foreHumArr = [];
+                for (var i = 0; i < weather.daily.length; i++){
+                    foreHumArr.push(weather.daily[i].humidity)
+                }
+
+                var foreIconArr = [];
+                for (var i = 0; i < weather.daily.length; i++){
+                    foreIconArr.push(weather.daily[i].weather[0].icon)
+                }
+                displayForecast(foreDateArr, foreTempArr, foreWindArr, foreHumArr, foreIconArr)
             })
         }
         else {
@@ -62,11 +94,27 @@ var displayJumbotron = function(temp, wind, hum, uv, date, icon) {
     uvEl.textContent = uv;
 }
 
-// var displayForecast = function (date, temp, wind, hum) {
-//     const foreTemp = document.querySelectorAll('.fore-temp');
-//     const foreWind = document.querySelectorAll('.fore-wind')
-//     const foreDate = document.querySelectorAll('.fore-date')
-//     const foreHum = document.querySelectorAll('.fore-humidity')
-//     for (var i = 0; i < )
+var displayForecast = function (date, temp, wind, hum, icon) {
+    const foreIcon = document.querySelectorAll('.fore-icon')
+    const foreTemp = document.querySelectorAll('.fore-temp');
+    const foreWind = document.querySelectorAll('.fore-wind')
+    const foreDate = document.querySelectorAll('.fore-date')
+    const foreHum = document.querySelectorAll('.fore-humidity')
+    for (var i = 0; i < foreTemp.length; i++) {
+        foreTemp[i].textContent = temp[i];
+    }
+    for (var i = 0; i < foreDate.length; i++) {
+        foreDate[i].textContent = date[i];
+    }
+    for (var i = 0; i < foreWind.length; i++) {
+        foreWind[i].textContent = wind[i];
+    }
+    for (var i = 0; i < foreHum.length; i++) {
+        foreHum[i].textContent = hum[i];
+    }
+    for (var i = 0; i < foreIcon.length; i++) {
+        foreIcon[i].innerHTML = '<img src="http://openweathermap.org/img/wn/' + icon[i] + '@2x.png" width="50px">';
+    }
+    
 
-// }
+}
